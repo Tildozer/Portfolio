bb024712d620303546db4003e992644916408fa7
tildozer <actninswitch@gmail.com>
2025-03-19 15:38:14 -0600

    done with the website till I have to add more. now I just need to get a favicon and host this live.

--- app/_mainPageComponents/Experience.tsx
+++ app/_mainPageComponents/Experience.tsx
@@ -3,33 +3,12 @@ import { useEffect, useRef } from "react";
 import { Laptop, Controls, Text } from "./";
 import { useThree } from "@react-three/fiber";
 import { Color, PerspectiveCamera, Group } from "three";
-// import { useDarkMode } from "../_providers/DarkModeProvider";
 import { ContactShadows, Environment } from "@react-three/drei";
 
 const Experience = () => {
-  // const { darkMode } = useDarkMode();
   const camera = useThree((state) => state.camera) as PerspectiveCamera;
-
-  // const darkBackground = new Color("#241a1a");
-  // const lightBackground = new Color("#f0f0f0");
-  // const backgroundColor = darkMode ? darkBackground : lightBackground;
-
   const background = useRef<Color>(null);
   const text = useRef<Group>(null);
-  console.log(text);
-
-  // useEffect(() => {
-  //   if (background.current !== backgroundColor) {
-  //     gsap.to(background.current, {
-  //       r: backgroundColor.r,
-  //       g: backgroundColor.g,
-  //       b: backgroundColor.b,
-  //       duration: 0.5,
-  //     });
-  //   }
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [darkMode]);
 
   useEffect(() => {
     const handleResize = () => {
--- app/_mainPageComponents/Text.tsx
+++ app/_mainPageComponents/Text.tsx
@@ -1,6 +1,5 @@
-import gsap from "gsap";
 import * as THREE from "three";
-import { useEffect, useRef, forwardRef } from "react";
+import { forwardRef } from "react";
 import { extend, useFrame } from "@react-three/fiber";
 import { useDarkMode } from "../_providers/DarkModeProvider";
 import { FontLoader, TextGeometry } from "three/examples/jsm/Addons.js";
@@ -11,8 +10,6 @@ import random2D from "./_shaders/includes/random2D.glsl";
 
 extend({ TextGeometry });
 
-// Remove the Props type that includes ref
-// Add forwardRef wrapper
 const Text = forwardRef<THREE.Group>((props, ref) => {
   const { darkMode } = useDarkMode();
   const fontData = {
@@ -23,15 +20,11 @@ const Text = forwardRef<THREE.Group>((props, ref) => {
     },
   };
 
-  const materialRef = useRef<THREE.MeshBasicMaterial>();
-
   const font = new FontLoader().parse(fontData);
 
   const lightColor = new THREE.Color(0xea580c);
   const darkColor = new THREE.Color(0x6fc5fd);
   const color = darkMode ? darkColor : lightColor;
-  const material = new THREE.MeshBasicMaterial({ color });
-  materialRef.current = material;
 
   const processedVertexShader = vertexShader.replace(
     "#include ../includes/random2D",
@@ -49,42 +42,27 @@ const Text = forwardRef<THREE.Group>((props, ref) => {
     blending: THREE.AdditiveBlending,
   });
 
-  useEffect(() => {
-    if (materialRef.current) {
-      gsap.to(materialRef.current.color, {
-        r: color.r,
-        g: color.g,
-        b: color.b,
-        duration: 0.5,
-      });
-    }
-
-    console.log(holographicMaterial);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [darkMode]);
-
   useFrame(({ clock }) => {
     const elapsedTime = clock.getElapsedTime();
-    holographicMaterial.uniforms.uTime.value = elapsedTime * 0.5;
+    holographicMaterial.uniforms.uTime.value = elapsedTime * 0.75;
   });
 
   return (
     <group ref={ref} position={[-3, 2.2, 0]} rotation={[-0.1, 0.9, -0.1]}>
       <mesh material={holographicMaterial}>
         <textGeometry
-          args={["Discover", { font, size: 0.5, height: 0.1, depth: 0.1 }]}
+          args={["Discover", { font, size: 0.75, height: 0.1, depth: 0.1 }]}
         />
       </mesh>
       <mesh position={[0, -0.5, 0]} material={holographicMaterial}>
         <textGeometry
-          args={["my work", { font, size: 0.5, height: 0.1, depth: 0.15 }]}
+          args={["my work", { font, size: 0.75, height: 0.1, depth: 0.15 }]}
         />
       </mesh>
     </group>
   );
 });
 
-// Add display name for better debugging
 Text.displayName = "Text";
 
 export default Text;
