c8e1f8dece5c466ed472f3d3999e6e1c548a18de
tildozer <actninswitch@gmail.com>
2025-05-25 16:12:54 -0600

    have window for about finder created and dragging to all the right spots

--- app/_mainPageComponents/_screenComponents/AboutFinder.tsx
+++ app/_mainPageComponents/_screenComponents/AboutFinder.tsx
@@ -1,9 +1,49 @@
-import React from "react";
-import { useLaptopInfo } from "@/app/_providers/LaptopInfoProvider";
+import { useEffect, useState } from "react";
+import { useLaptopInfo } from "../../_providers/LaptopInfoProvider";
+import { WindowBar } from "./";
+import Draggable from "react-draggable";
 
 const AboutFinder = () => {
-  const { setters, maxZIndex } = useLaptopInfo();
-  return <div>AboutFinder</div>;
+  const [zIndex, setZIndex] = useState(0);
+  const {
+    scale,
+    state: { maxZIndex },
+    setters: { setMaxZIndex, setShowAboutFinder },
+  } = useLaptopInfo();
+
+  const handleMouseDown = () => {
+    setZIndex(maxZIndex + 1);
+    setMaxZIndex(maxZIndex + 1);
+  };
+
+  useEffect(() => {
+    setZIndex(maxZIndex + 1);
+    setMaxZIndex(maxZIndex + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Draggable
+      scale={scale}
+      bounds={{ left: -422, top: -256, right: 570, bottom: 190 }}
+      onMouseDown={handleMouseDown}
+      handle=".windowBar"
+    >
+      <div
+        className="absolute left-[30%] top-[30%] h-[30rem] w-[26rem] animate-expandBox bg-white text-6xl text-black shadow-xl shadow-black"
+        style={{ zIndex: zIndex }}
+      >
+        <WindowBar
+          callback={() => setShowAboutFinder(false)}
+          className="h-[2.5rem]"
+        >
+          <span className="mb-auto ml-4 mt-auto text-2xl">
+            About this Developer
+          </span>
+        </WindowBar>
+      </div>
+    </Draggable>
+  );
 };
 
 export default AboutFinder;
